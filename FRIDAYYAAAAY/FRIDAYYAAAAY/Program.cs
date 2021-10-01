59ebcb7e9b3b5bc4dc41a9b66bcec307f9969f79
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-01 15:12:18 +0100

    excercise with queue and lists - not complete

---
+++ FRIDAYYAAAAY/FRIDAYYAAAAY/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+namespace FRIDAYYAAAAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //==============LIST======================//
+            List<User> users = new List<User>();
+
+            User user1 = new User("Leonardo", "Turtle", "Leo", 7, "Leo@gmail.com");
+
+            User user2 = new User("Raphael", "Turtle", "Raph", 7, "Raph@gmail.com");
+
+            User user3 = new User("Donatello", "Turtle", "Don", 9, "Don@gmail.com");
+
+            User user4 = new User("Michaelangelo", "Turtle", "Mike", 9, "Mike@gmail.com");
+
+            users.Add(user1);
+            users.Add(user2);
+            users.Add(user3);
+            users.Add(user4);
+
+            Console.WriteLine("List test with foreach loop");
+
+            foreach (var User in users)
+            {
+                User.PrintInfo();
+            }
+
+            Console.WriteLine();
+
+            //==============QUEUE======================//
+            Console.WriteLine("Queue test with foreach loop");
+
+            Queue<User> userque = new Queue<User>();
+
+            userque.Enqueue(user1);
+            userque.Enqueue(user2);
+            userque.Enqueue(user3);
+            userque.Enqueue(user4);
+
+            for (int i = 0; i < userque.Count; i++)
+            {
+                User userAtFrontOfQueue = userque.Dequeue();
+                if (userAtFrontOfQueue != null) i--;
+                Console.WriteLine(userAtFrontOfQueue.GetInfo());
+            }
+
+
+
+        }
+    }
+}
---
+++ FRIDAYYAAAAY/FRIDAYYAAAAY/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRIDAYYAAAAY
+{
+    class User
+    {
+        public string forename;
+        public string surname;
+        public string nickname;
+        public int age;
+        public string email;
+
+        public User(string forename, string surname, string nickname, int age, string email)
+        {
+            this.forename = forename;
+            this.surname = surname;
+            this.nickname = nickname;
+            this.age = age;
+            this.email = email;
+        }
+
+        public User()
+        {
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"User: {forename} {surname} , nickname: {nickname}, age: {age}, email: {email}");
+        }
+
+
+        public string GetInfo()
+        {
+            return $"User: {forename} {surname} , nickname: {nickname}, age: {age}, email: {email}";
+        }
+
+
+
+
+    }
+}
