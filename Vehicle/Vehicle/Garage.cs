9d8117c270c3a96d6fffd1443f52569f2addf165
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-05 14:15:42 +0100

    garage list added, and interface

---
+++ Vehicle/Vehicle/Garage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    //    Create a Garage class that can store a list of Vehicle with methods for the following:
+    //Adding a new Vehicle to the garageRemoving a Vehicle from the garage by idRemove all Vehicles
+    //of a certain typeEmpty garage which removes all vehicles
+    //The methods for adding and removing vehicles should call the vehicles ToString() method
+
+    class Garage
+    {
+        //------list set up
+       private IList<Vehicle> myGarage = new List<Vehicle>();
+
+        //public List<Vehicle> GetList()
+        //{
+        //    return (List<Vehicle>)myGarage;
+        //}
+
+        //---Adding method
+        public void AddVehicle(Vehicle vehicle)
+        {
+            myGarage.Add(vehicle);
+        }
+
+        public Vehicle getVehicleByID(int vehicle)
+        {
+            return myGarage[vehicle]        ;
+        }
+
+
+    }
+}
